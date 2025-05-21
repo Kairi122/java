f30103fcf52d075b241dd4783b716aa96016fe01
Kairi122 <kairi_shimizu@aiful.co.jp>
2025-05-21 09:58:18 +0900

    オブジェクトの練習

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("HelloWorld!");
+        //社員1を作る
+        object shain1 = new object();
+        shain1.setId(20252029);
+        shain1.setName("Kairi");
+        shain1.setSei("Shimizu");
+        shain1.setNen(2000);
+
+        System.out.println(shain1.getId()+":"+shain1.getName()+":"+shain1.getSei()+":"+shain1.getNen());
     }
 }
---
+++ object.java
@@ -0,0 +1,38 @@
+public class object {
+    private int id;
+    private String name;
+    private String sei;
+    private int nen;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+        public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+        public String getSei(){
+        return sei;
+    }
+
+    public void setSei(String sei){
+        this.sei = sei;
+    }
+
+        public int getNen(){
+        return nen;
+    }
+
+    public void setNen(int nen){
+        this.nen = nen;
+    }
+}
