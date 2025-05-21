29db30eefdfb89e7b95b3d7038aa63ff2b6b842a
Kairi122 <kairi_shimizu@aiful.co.jp>
2025-05-21 10:47:39 +0900

    オブジェクトとリストの練習

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,28 @@
+import java.util.ArrayList;
+
 public class HelloWorld{
-    public static void main(String[] args){
+        public static void main(String[] args){
+        ArrayList<object> shainList = new ArrayList<>();
         //社員1を作る
         object shain1 = new object();
         shain1.setId(20252029);
         shain1.setName("Kairi");
         shain1.setSei("Shimizu");
         shain1.setNen(2000);
+        shainList.add(shain1);
 
-        System.out.println(shain1.getId()+":"+shain1.getName()+":"+shain1.getSei()+":"+shain1.getNen());
+
+        object shain2 = new object();
+        shain2.setId(20252031);
+        shain2.setName("John");
+        shain2.setSei("Smith");
+        shain2.setNen(1999);
+        shainList.add(shain2);
+
+        System.out.println(shainList.get(0).getName());
+
+        for(object obj : shainList){
+            System.out.println(obj.getId()+":"+obj.getName()+":"+obj.getNen()+":"+obj.getSei());
+        }
     }
 }
